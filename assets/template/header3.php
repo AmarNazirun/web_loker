88ee196f10fa10d6e9f6006520b4a0121c1ffbf7
Amar Nazirun <amarnazirun28@gmail.com>
2025-06-06 21:58:44 +0900

    tahap awal mulai menata

---
+++ assets/template/header3.php
@@ -0,0 +1,56 @@
+<header id="header" class="header">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Website Loker</a>
+
+            <!-- Tombol untuk offcanvas (tampil hanya pada layar lg ke bawah) -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Offcanvas Container -->
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex align-items-center justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Lowongan Online</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tips Loker</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cari Lowongan</a>
+                        </li>
+                        <!-- <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li> -->
+                    </ul>
+                    <!-- <form class="d-flex align-items-center justify-content-center" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form> -->
+                    <!-- login -->
+                    <a class="btn btn-primary ms-2" href="login.php">Registrasi</a>
+                    <a class="btn btn-primary ms-2" href="login.php">Login</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>

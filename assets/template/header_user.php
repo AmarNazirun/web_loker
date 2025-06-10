d18ccea47cb3a330b7b4f9b6d4ce65b942bc8032
Amar Nazirun <amarnazirun28@gmail.com>
2025-06-10 11:50:15 +0900

    Perbaiki tautan profil pengguna dengan mengganti user-profile.php menjadi user_profile.php

--- assets/template/header_user.php
+++ assets/template/header_user.php
@@ -58,7 +58,7 @@
                             <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
 
                                 <li>
-                                    <a class="dropdown-item d-flex align-items-center" href="user-profile.php">
+                                    <a class="dropdown-item d-flex align-items-center" href="user_profile.php">
                                         <i class="bi bi-person"></i>
                                         <span>My Profile</span>
                                     </a>
